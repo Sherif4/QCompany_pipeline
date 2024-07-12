2e4f8150c250affee7255c47c0abcb46a069db10
sherif4 <dr.sherif.ashraf4@gmail.com>
2024-07-13 01:53:30 +0300

    Organization

--- schedule_script.sh
+++ schedule_script.sh
@@ -4,7 +4,7 @@ export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
 
-/home/itversity/.local/bin/papermill /home/itversity/itversity-material/Retail_pipeline_project/ingestion_script.ipynb /home/itversity/itversity-material/retail_pipeline_project/papermill_logs/ingestion_output.json \
-	&& /home/itversity/.local/bin/papermill  /home/itversity/itversity-material/Retail_pipeline_project/Silver_layer_transformation.ipynb /home/itversity/itversity-material/Retail_pipeline_project/papermill_logs/Silver_output.json \
-	&& /home/itversity/.local/bin/papermill  /home/itversity/itversity-material/Retail_pipeline_project/Gold_layer_transformation.ipynb /home/itversity/itversity-material/Retail_pipeline_project/papermill_logs/Gold_output.json
+/home/itversity/.local/bin/papermill /home/itversity/itversity-material/Retail_pipeline_project/Scripts/ingestion_script.ipynb /home/itversity/itversity-material/retail_pipeline_project/papermill_logs/ingestion_output.json \
+	&& /home/itversity/.local/bin/papermill  /home/itversity/itversity-material/Retail_pipeline_project/Scripts/Silver_layer_transformation.ipynb /home/itversity/itversity-material/Retail_pipeline_project/papermill_logs/Silver_output.json \
+	&& /home/itversity/.local/bin/papermill  /home/itversity/itversity-material/Retail_pipeline_project/Scripts/Gold_layer_transformation.ipynb /home/itversity/itversity-material/Retail_pipeline_project/papermill_logs/Gold_output.json
 
