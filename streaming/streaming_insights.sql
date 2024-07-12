b3ff192ec829933fa57f1a8ea073fd664ed0207e
hassanhosny2001 <hhosny2050@gmail.com>
2024-07-12 17:51:38 +0300

    added readme

--- streaming/streaming_insights.sql
+++ streaming/streaming_insights.sql
@@ -1,31 +1,31 @@
 -- Select the date of the view, total number of views per product per day
 SELECT 
-    DATE(timestamp) AS viewDate, 
+    DATE(`timestamp`) AS viewDate, 
     COUNT(*) AS totalViews,
     productId
 FROM 
     product_view
 GROUP BY 
-    DATE(timestamp), 
+    DATE(`timestamp`), 
     productId;
 
 -- Select the date, total quantity added to cart per day
 SELECT 
-    DATE(timestamp) AS cartDate, 
+    DATE(`timestamp`) AS cartDate, 
     SUM(quantity) AS totalQuantityAdded
 FROM 
     total_add_cart_per_day
 GROUP BY 
-    DATE(timestamp);
+    DATE(`timestamp`);
 
 -- Select the date, total number of clicks per day
 SELECT 
-    DATE(timestamp) AS clickDate, 
+    DATE(`timestamp`) AS clickDate, 
     COUNT(*) AS totalClicks
 FROM 
     total_number_per_day
 GROUP BY 
-    DATE(timestamp);
+    DATE(`timestamp`);
 
 -- Select the algorithm used for recommendation, total number of clicks per algorithm
 SELECT 
@@ -50,10 +50,10 @@ LIMIT 5;
 
 -- Select the date of sale, total quantity sold, and total sales amount per day
 SELECT 
-    DATE(timestamp) AS saleDate, 
+    DATE(`timestamp`) AS saleDate, 
     SUM(quantity) AS totalQuantitySold,
     SUM(totalAmount) AS totalSalesAmount
 FROM 
     purchases
 GROUP BY 
-    DATE(timestamp);
+    DATE(`timestamp`);
